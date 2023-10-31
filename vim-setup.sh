6c48f0e99c0c2a36a55aa04ca2861fc5d822687a
Ian Oxley <ijoxley@gmail.com>
2023-10-31 17:25:59 +0000

    Add the other/start plugins

--- vim-setup.sh
+++ vim-setup.sh
@@ -1,3 +1,14 @@
 #! /usr/bin/env bash
 
 mkdir -p ~/.vim/pack/{other,syntax,themes}/{opt,start}
+
+cd ~/.vim/pack/other/start
+git clone git@github.com:mileszs/ack.vim.git ack.vim
+git clone https://github.com/w0rp/ale.git ale
+git clone https://github.com/junegunn/fzf.vim.git fzf.vim
+git clone https://github.com/itchyny/lightline.vim lightline.vim
+git clone git@github.com:maximbaz/lightline-ale.git lightline-ale
+git clone git@github.com:preservim/nerdtree.git nerdtree
+git clone git@github.com:tpope/vim-fugitive.git vim-fugitive
+git clone git@github.com:jamessan/vim-gnupg.git vim-gnupg
+git clone git@github.com:tpope/vim-unimpaired.git vim-unimpaired
