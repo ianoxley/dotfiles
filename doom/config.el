0eabc36a0bd7c284ef369b4fe5632105e9684c9a
Ian Oxley <ijoxley@gmail.com>
2024-03-04 15:41:49 +0000

    Fix font sizes

--- doom/config.el
+++ doom/config.el
@@ -21,8 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "Ubuntu Mono" :size 18))
-     ;; doom-variable-pitch-font (font-spec :family "Fira Sans" :size 20))
+(setq doom-font (font-spec :family "Ubuntu Mono" :size 18)
+      doom-variable-pitch-font (font-spec :family "Ubuntu Mono" :size 30))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
