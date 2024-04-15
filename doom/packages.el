7a036a90211da27e7641fd56c3b6d99f8d2d8a81
Ian Oxley <ijoxley@gmail.com>
2024-04-15 11:50:36 +0100

    Add helm to packages.el

--- doom/packages.el
+++ doom/packages.el
@@ -14,6 +14,7 @@
 (package! org-roam)
 (package! org-roam-ui)
 (package! org-super-agenda)
+(package! helm)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
