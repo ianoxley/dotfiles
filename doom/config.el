9b5f1900b891e61eb0b38ea18a2eb0542f026d18
Ian Oxley <ian.oxley@sage.com>
2026-04-13 11:05:36 +0100

    Use Iosevka font for unicode symbols

--- doom/config.el
+++ doom/config.el
@@ -29,6 +29,8 @@
 (setq doom-font (font-spec :family "Fira Code" :size 18)
       doom-variable-pitch-font (font-spec :family "Alegreya" :size 24)
       doom-big-font (font-spec :family "Fira Code" :size 24))
+
+(set-fontset-font t 'unicode (font-spec :family "Iosevka") nil 'prepend)
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
